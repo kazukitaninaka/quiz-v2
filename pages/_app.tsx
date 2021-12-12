2da1c9c5ccc295f5670961df2538ae6010f5ec47
Kazuki Taninaka <onelive555@gmail.com>
2021-12-12 18:07:11 +0900

    migrate to chakra (_app.tsx)

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,15 +1,22 @@
 import "../styles/globals.css";
 import type { AppProps } from "next/app";
-import { ChakraProvider } from "@chakra-ui/react";
+import { ChakraProvider, Flex, Box } from "@chakra-ui/react";
 import Header from "../components/Header";
 import Footer from "../components/Footer";
+import { Container } from "@chakra-ui/react";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
     <ChakraProvider>
-      <Header />
-      <Component {...pageProps} />
-      <Footer />
+      <Flex direction="column" minH="100vh">
+        <Header />
+        <Box style={{ flex: "1" }}>
+          <Container maxW="container.sm">
+            <Component {...pageProps} />
+          </Container>
+        </Box>
+        <Footer />
+      </Flex>
     </ChakraProvider>
   );
 }
