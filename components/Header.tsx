e1fa160cbd6ea5efb9acb8a225b3e897d8a31c5f
Kazuki Taninaka <onelive555@gmail.com>
2021-12-12 18:42:50 +0900

    set text decoration to none

--- components/Header.tsx
+++ components/Header.tsx
@@ -4,13 +4,13 @@ import { Flex, Link, Text } from "@chakra-ui/react";
 export default function Header() {
   return (
     <Flex p={5} backgroundColor="blue.500" as="header">
-      <Link href="/">
+      <Link href="/" _hover={{ textDecoration: "none" }}>
         <Text fontSize="2xl" fontWeight="bold" color="white">
           Quiz
         </Text>
       </Link>
       <Flex alignItems="center" color="white">
-        <Link href="/ranking">
+        <Link href="/ranking" _hover={{ textDecoration: "none" }}>
           <Text fontSize="lg" fontWeight="bold" ml={8}>
             Ranking
           </Text>
