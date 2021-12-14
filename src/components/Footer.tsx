95102e55479afc6068d883ae9b45772bcf528d0c
Kazuki Taninaka <onelive555@gmail.com>
2021-12-14 13:45:13 +0900

    make codes 'prettier'

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,11 +1,9 @@
-import { Flex, Text } from "@chakra-ui/react";
+import { Flex, Text } from '@chakra-ui/react';
 
 export default function Footer() {
   return (
-    <Flex p={5} backgroundColor="blue.500" as="footer">
-      <Text color="white">
-        Copyrights &copy; 2021 Kazuki Taninaka. All rights reserved
-      </Text>
+    <Flex p={5} backgroundColor='blue.500' as='footer'>
+      <Text color='white'>Copyrights &copy; 2021 Kazuki Taninaka. All rights reserved</Text>
     </Flex>
   );
 }
--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -1,17 +1,17 @@
 // import { AppBar, Toolbar, Typography, Link } from "@material-ui/core";
-import { Flex, Link, Text } from "@chakra-ui/react";
+import { Flex, Link, Text } from '@chakra-ui/react';
 
 export default function Header() {
   return (
-    <Flex p={5} backgroundColor="blue.500" as="header">
-      <Link href="/" _hover={{ textDecoration: "none" }}>
-        <Text fontSize="2xl" fontWeight="bold" color="white">
+    <Flex p={5} backgroundColor='blue.500' as='header'>
+      <Link href='/' _hover={{ textDecoration: 'none' }}>
+        <Text fontSize='2xl' fontWeight='bold' color='white'>
           Quiz
         </Text>
       </Link>
-      <Flex alignItems="center" color="white" ml={8}>
-        <Link href="/ranking" _hover={{ textDecoration: "none" }}>
-          <Text fontSize="lg" fontWeight="bold">
+      <Flex alignItems='center' color='white' ml={8}>
+        <Link href='/ranking' _hover={{ textDecoration: 'none' }}>
+          <Text fontSize='lg' fontWeight='bold'>
             Ranking
           </Text>
         </Link>
