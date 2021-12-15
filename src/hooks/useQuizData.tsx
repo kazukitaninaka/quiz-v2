2f7d359b7ebd75c459f8d193509578b16d0abec3
Kazuki Taninaka <onelive555@gmail.com>
2021-12-15 18:05:23 +0900

    make swr immutable one

--- src/hooks/useQuizData.tsx
+++ src/hooks/useQuizData.tsx
@@ -1,18 +1,18 @@
-import { useEffect, useRef } from 'react';
-import useSWR from 'swr';
+import useSWRImmutable from 'swr/immutable';
 import { FetchedQuizData } from '../types';
 import shuffleArray from '../utils/shuffle';
+
 const fetcher = (url: string) => fetch(url).then((r) => r.json());
 
 export default function useQuizData(shouldFetch: boolean) {
-  const { data, error } = useSWR<FetchedQuizData>(
+  const { data, error } = useSWRImmutable<FetchedQuizData>(
     shouldFetch
       ? 'https://opentdb.com/api.php?amount=5&category=9&difficulty=easy&type=multiple&encode=url3986'
       : null,
     fetcher,
   );
 
-  const quizData = data?.results.map((quiz: QuizData) => ({
+  const quizData = data?.results.map((quiz) => ({
     ...quiz,
     answers: shuffleArray([...quiz.incorrect_answers, quiz.correct_answer]),
   }));
