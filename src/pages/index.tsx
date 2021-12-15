ca9fcdff041ec9fe48c1bffc16cadbbe9d5eacdf
Kazuki Taninaka <onelive555@gmail.com>
2021-12-15 18:11:02 +0900

    refactor return statement in index.tsx

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,12 @@
 import { useState } from 'react';
-import { Box, CircularProgress } from '@material-ui/core';
 import QuestionCard from '../components/QuestionCard';
 import Start from '../components/Start';
 import Result from '../components/Result';
 import { db } from '../../firebase';
-import shuffleArray from '../utils/shuffle';
-import { PlayerData, QuizData, QuizInfo } from '../types';
+import { PlayerData } from '../types';
 import { ref, set } from 'firebase/database';
 import useQuizData from '../hooks/useQuizData';
+import { Center, CircularProgress, Text } from '@chakra-ui/react';
 
 export default function Home() {
   const [gameStarted, setGameStarted] = useState<boolean>(false);
@@ -35,31 +34,35 @@ export default function Home() {
     setQuestionNum((prev) => prev + 1);
   };
 
-  let content;
-  if (!gameStarted && !score) {
-    content = (
+  if (!gameStarted) {
+    return (
       <Start startGame={startGame} setPlayerData={setPlayerData} playersName={playerData.name} />
     );
-  } else if (isError) {
-    content = <div>error</div>;
-  } else if (isLoading || !quiz) {
-    content = (
-      <Box textAlign='center'>
-        <CircularProgress />
-      </Box>
-    );
-  } else if (questionNum - 1 >= quiz.length) {
-    content = <Result score={score} playerData={playerData} />;
-  } else {
-    content = (
-      <QuestionCard
-        questionData={quiz[questionNum - 1]}
-        questionNum={questionNum}
-        setScore={setScore}
-        setQuestionNum={setQuestionNum}
-        finishGame={finishGame}
-      />
+  }
+
+  if (isLoading) {
+    return (
+      <Center>
+        <CircularProgress isIndeterminate />
+      </Center>
     );
   }
-  return <>{content}</>;
+
+  if (isError) {
+    return <Text>Error occured...</Text>;
+  }
+
+  if (questionNum - 1 >= quiz!.length) {
+    return <Result score={score} playerData={playerData} />;
+  }
+
+  return (
+    <QuestionCard
+      questionData={quiz[questionNum - 1]}
+      questionNum={questionNum}
+      setScore={setScore}
+      setQuestionNum={setQuestionNum}
+      finishGame={finishGame}
+    />
+  );
 }
