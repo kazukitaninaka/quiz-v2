c55447ae134290b865288bccbfa4dca235d2d315
Kazuki Taninaka <onelive555@gmail.com>
2021-12-16 18:55:34 +0900

    switch swr to fetch api

--- src/hooks/useQuizData.tsx
+++ src/hooks/useQuizData.tsx
@@ -1,25 +1,46 @@
-import useSWRImmutable from 'swr/immutable';
-import { FetchedQuizData } from '../types';
+import { useEffect, useState } from 'react';
+import { FetchedQuizData, QuizInfo } from '../types';
 import shuffleArray from '../utils/shuffle';
 
-const fetcher = (url: string) => fetch(url).then((r) => r.json());
-
 export default function useQuizData(shouldFetch: boolean) {
-  const { data, error } = useSWRImmutable<FetchedQuizData>(
-    shouldFetch
-      ? 'https://opentdb.com/api.php?amount=5&category=9&difficulty=easy&type=multiple&encode=url3986'
-      : null,
-    fetcher,
-  );
+  const [quiz, setQuiz] = useState<QuizInfo[]>([]);
+  const [isLoading, setIsLoading] = useState<boolean>(false);
+  const [isError, setIsError] = useState<boolean>(false);
 
-  const quizData = data?.results.map((quiz) => ({
-    ...quiz,
-    answers: shuffleArray([...quiz.incorrect_answers, quiz.correct_answer]),
-  }));
+  async function fetchQuiz(): Promise<FetchedQuizData> {
+    return fetch(
+      'https://opentdb.com/api.php?amount=5&category=9&difficulty=easy&type=multiple&encode=url3986',
+    ).then((res) => res.json());
+  }
+
+  useEffect(() => {
+    if (!shouldFetch) return;
+    setIsLoading(true);
+    fetchQuiz()
+      .then((data) => {
+        const quizData = data.results.map((quiz) => ({
+          ...quiz,
+          answers: shuffleArray([...quiz.incorrect_answers, quiz.correct_answer]),
+        }));
+        setQuiz(quizData);
+        setIsLoading(false);
+        setIsError(false);
+      })
+      .catch(() => {
+        setIsError(true);
+      })
+      .finally(() => {
+        setIsLoading(false);
+      });
+
+    return () => {
+      setQuiz([]);
+    };
+  }, [shouldFetch]);
 
   return {
-    quiz: quizData,
-    isLoading: !error && !data,
-    isError: error,
+    quiz,
+    isLoading,
+    isError,
   };
 }
